bc9079cee580c798c46c2a7960495b6a916071c9
HallworthMT <mhallworth@vtecostudies.org>
2023-04-18 10:20:21 -0400

    upload files

---
+++ welcome_AWS.r
@@ -0,0 +1,40 @@
+####################################################################################################
+#
+#                      Welcome to Biodiversity Data at Your Fingertips: in the cloud
+#
+####################################################################################################
+
+# This workshop will introduce participants to the Vermont Atlas of Life's community science data 
+# stored at the Global Biodiversity Information Facility (GBIF) submitted through various platforms
+# such as eBird.org, iNaturalist.org, OdonataCentral.org, and eButterfly.org. Participants will get
+# hands-on experience analyzing community science observations, from creating species lists to 
+# mapping species distributions. The objective of the workshop is to introduce and 
+# provide participants with working examples of how to use R to access, analyze and plot community 
+# science data.
+
+# Most of the spatial layers that we'll use in the workshop are already available on this machine. 
+# The "../Spatial_Layers" folder was intentionally placed so that the code for the workshop doesn't 
+# need to be altered. While you can't see the actual folder in your 'home' drive, it's there just
+# one folder up hence the "../" in the file code. 
+
+# You're currently running RStudio through Amazon Web Services. All participants are sharing the
+# memory (72gb) and spatial layers. Please be mindful of that before writing / removing files from the
+# "../Spatial_Layers" folder. 
+
+# RUNNING CODE - To run lines of code you need to physically press the Run key in the upper right
+# of the Scripts window. The familiar shortcuts ('ctrl-r' for example) don't work. You will notice
+# that the page refreshes if you use the shortcut but the code won't run. 
+
+# BE SURE TO EXPORT YOUR SCRIPTS AT THE END OF THE WORKSHOP!!
+# I'll be stopping Amazon Web Services after the course is finished. At that point, the scripts you 
+# save on the server will be deleted. You can download any files you save to the server by expanding
+# the 'more' button in the Files tab. Select the file/s you want to download and click 'Export'. 
+
+# Good luck, happy coding & I hope you learn something new! 
+
+# Mike
+
+
+
+
+
